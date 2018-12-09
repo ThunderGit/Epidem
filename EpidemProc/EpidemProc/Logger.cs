a34c9b9485195eb65ef55661e86e143a999e0b2b
kkrutenyuk <kkrutenyuk@jaggedpeak.com>
2018-12-10 01:47:43 +0200

    Logger update

--- EpidemProc/EpidemProc/Logger.cs
+++ EpidemProc/EpidemProc/Logger.cs
@@ -11,21 +11,62 @@ using EpidemProc.MedPart;
 using EpidemProc.Scientist;
 using EpidemProc.MilPart;
 using EpidemProc.PolPart;
+using EpidemProc.Model.Log;
 
 namespace EpidemProc
 {
 	class Logger
 	{
 
-		private static void Log_Infected()
+		private static void Log_Infected(long iter, Citizen[] _Citizens, Police[] _Police,
+		Hospital[] _Hospital, Millitary[] _Military, Facture[] _Facture, Home[] _home)
 		{
 
 		}
+		private static void Log_Global(int iter, int day, int hour, Citizen[] _Citizens, Policeman[] policemen, Doctor[] doctors, Troop[] troops, Weather weather, int status, int researchProgress, int countOfDeath)
+		{
+			LogGlobal LG = new LogGlobal
+			{
+				Id = 0,
+				Iteration = iter,
+				CurrentHour = hour,
+				CurrentDay = day,
+				RegPopulation = _Citizens.Length,
+				CountOfInfected = 0,
+				CountOfHealthy = 0,
+				CountOfDeath = countOfDeath,
+				CountOfPolicemans = policemen.Length,
+				CountOfSoldiers = troops.Length,
+				CountOfDoctors = doctors.Length,
+				Temperature = weather.t,
+				 Wet = weather.wet,
+				Research = researchProgress,
+				RegStatus = status,
+			};
 
+		}
+		private static void Log_Med_Stat(Citizen[] _Citizens, int iter)
+		{
+			int countInf = 0;
+			int countCrit = 0;
+			for (int i = 0; i < _Citizens.Length; i++)
+			{
+				if (_Citizens[i].SickLeave) countInf++;
+				if (_Citizens[i].Hospitalized) countCrit++;
+			}
+			LogMedStatistic LMS = new LogMedStatistic
+			{
+				Id = 0,
+				Iteration = iter,
+				CountOfHealthy = _Citizens.Length - countInf,
+				CountOfInfected = countInf,
+				CountOfCritical = countCrit
+			};
+		}
 
 		public static void Run(Citizen[] _Citizens, Policeman[] P, Doctor[] D, Troop[] T, Police[] _Police,
 	Hospital[] _Hospital, Millitary[] _Military, Facture[] _Facture, Home[] _home, int day, int hour, int totalDay, int currentDay,
-	int status, int researchProgress, int countOfDeath, Virus virus, Weather weather, MedicinePart med, Economic Econimic)
+	int status, int researchProgress, int countOfDeath, int iter, Virus virus, Weather weather, MedicinePart med, Economic Econimic)
 		{ 
 			
 		}

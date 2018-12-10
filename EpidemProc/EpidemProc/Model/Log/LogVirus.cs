b5e082554fa687db437da29ca18ec8d80c8bc2a7
Andrew Ivlev <aivlev8@gmail.com>
2018-12-10 03:05:26 +0200

    Update LogVirus.cs

--- EpidemProc/EpidemProc/Model/Log/LogVirus.cs
+++ EpidemProc/EpidemProc/Model/Log/LogVirus.cs
@@ -1,4 +1,5 @@
-﻿using System.Data;
+﻿using System;
+using System.Data;
 using System.Data.SqlClient;
 
 namespace EpidemProc.Model.Log
@@ -24,5 +25,100 @@ namespace EpidemProc.Model.Log
 		public int sensoryDamaged { get; set; }
 		public int lyphaticDamaged { get; set; }
 		public int immunityDamaged { get; set; }
-	}
+        public void SaveToDB()
+        {
+
+            int iter = this.Iteration;
+            int D = this.Difficult;
+            int MinInT = this.MinInfectT;
+            int MaxInT = this.MaxInfectT;
+            int MinComfT = this.MinComfortT;
+            int MaxComfT = this.MaxComfortT;
+            int WP = this.WetProtect;
+            int SkelD = this.skeletonDamaged;
+            int MD = this.muscleDamaged;
+            int ResD = this.respiratoryDamaged;
+            int CD = this.circulatoryDamaged;
+            int DiuD = this.diureticDamaged;
+            int DIfD = this.digestiveDamaged;
+            int nD = this.nervousDamaged;
+            int RepD = this.reproductiveDamaged;
+
+            int SensD = this.sensoryDamaged;
+            int LD = this.lyphaticDamaged;
+            int ImD = this.immunityDamaged;
+
+
+            SqlConnectionStringBuilder connect =
+                            new SqlConnectionStringBuilder();
+            connect.InitialCatalog = "PANDEMIC_INC";
+            connect.DataSource = @"DESKTOP-SH16UUG";
+            connect.ConnectTimeout = 120;
+            connect.IntegratedSecurity = true;
+
+
+            // Создание открытого подключения
+            using (SqlConnection cn = new SqlConnection())
+            {
+                try
+                {
+                    cn.ConnectionString = connect.ConnectionString;
+                    cn.Open();
+                    System.Console.WriteLine("SUCCESS!");//уберёшь потом если что
+
+
+                    //пошла жара
+
+                    SqlCommand command = new SqlCommand();
+                    command.Connection = cn;
+                    command.CommandType = CommandType.Text;
+                    //Insert Data
+
+                    using (SqlCommand cmd = new SqlCommand(@"INSERT INTO 
+TABLE VIRUS (ITERRATION,DIFFICULT,MININFECTT,MAXINFECTT,MINCOMFORTT,
+MAXCOMFORTT,WETPROTECT,SKELETONDAMAGED,MUSCLEDAMAGED,RESPIRATORYDAMAGED,
+CIRCULATORYDAMAGED,DIURETICDAMAGED,DIGESTIVEDAMAGED,
+NERVOUSDAMAGED,REPRODUCTIVEDAMAGED,SENSORYDAMAGED,LYPHATICDAMAGED,IMMUNITYDAMAGED) 
+VALUES ( @iter, @D, @MinInT,@MaxInT,@MinComfT,@MaxComfT,@WP,@SkelD,@MD,@ResD,@CD,@DiuD,@DIfD,@nD,
+@RepD,@SensD,@LD,@ImD)", cn))
+                    {
+
+                        cmd.Parameters.AddWithValue("@iter", iter);
+                        cmd.Parameters.AddWithValue("@D", D);
+                        cmd.Parameters.AddWithValue("@MinInT", MinInT);
+                        cmd.Parameters.AddWithValue("@MaxInT", MaxInT);
+                        cmd.Parameters.AddWithValue("@MinComfT", MinComfT);
+                        cmd.Parameters.AddWithValue("@MaxComfT", MaxComfT);
+                        cmd.Parameters.AddWithValue("@WP", WP);
+                        cmd.Parameters.AddWithValue("@SkelD", SkelD);
+                        cmd.Parameters.AddWithValue("@MD", MD);
+                        cmd.Parameters.AddWithValue("@ResD", ResD);
+                        cmd.Parameters.AddWithValue("@CD", CD);
+                        cmd.Parameters.AddWithValue("@DiuD", DiuD);
+                        cmd.Parameters.AddWithValue("@DIfD", DIfD);
+                        cmd.Parameters.AddWithValue("@nD", nD);
+                        cmd.Parameters.AddWithValue("@RepD", RepD);
+                        cmd.Parameters.AddWithValue("@SensD", SensD);
+                        cmd.Parameters.AddWithValue("@LD", LD);
+                        cmd.Parameters.AddWithValue("@ImD", ImD);
+
+                        cmd.ExecuteNonQuery();
+                    }
+
+                    //прошла жара
+
+
+                    cn.Close();
+                }
+                catch (Exception ex)
+                {
+                    System.Console.WriteLine("Cannot connect to db\n\n" + ex.Message);
+                }
+            }
+
+        }
+
+
+
+    }
 }

cd9c79cac3900421f06c4c6211070dcbad3fc9a2
Andrew Ivlev <aivlev8@gmail.com>
2018-12-10 03:05:31 +0200

    Update LogInfected.cs

--- EpidemProc/EpidemProc/Model/Log/LogInfected.cs
+++ EpidemProc/EpidemProc/Model/Log/LogInfected.cs
@@ -1,4 +1,5 @@
-﻿using System.Data;
+﻿using System;
+using System.Data;
 using System.Data.SqlClient;
 
 namespace EpidemProc.Model.Log
@@ -11,5 +12,71 @@ namespace EpidemProc.Model.Log
 		public int Y { get; set; }
 		public int CountOfInfected { get; set; }
 		public int CountOfHealthy { get; set; }
-	}
+        
+        public void SaveToDB()
+        {
+
+            int iter = this.Iteration;
+            int x = this.X;
+            int y = this.Y;
+           
+            int CofH = this.CountOfHealthy;
+            int CofI = this.CountOfInfected;
+   
+
+            SqlConnectionStringBuilder connect =
+                            new SqlConnectionStringBuilder();
+            connect.InitialCatalog = "PANDEMIC_INC";
+            connect.DataSource = @"DESKTOP-SH16UUG";
+            connect.ConnectTimeout = 120;
+            connect.IntegratedSecurity = true;
+
+
+            // Создание открытого подключения
+            using (SqlConnection cn = new SqlConnection())
+            {
+                try
+                {
+                    cn.ConnectionString = connect.ConnectionString;
+                    cn.Open();
+                    System.Console.WriteLine("SUCCESS!");//уберёшь потом если что
+
+
+                    //пошла жара
+
+                    SqlCommand command = new SqlCommand();
+                    command.Connection = cn;
+                    command.CommandType = CommandType.Text;
+                    //Insert Data
+
+                    using (SqlCommand cmd = new SqlCommand(@"INSERT INTO 
+TABLE LOG_GLOBAL (ITERRATION, X,Y,COUNT_OF_INFECTED, COUNT_OF_HEALTHY) 
+VALUES ( @iter, @x, @y,@CofI,@CofH)", cn))
+                    {
+
+                        cmd.Parameters.AddWithValue("@iter", iter);
+                        cmd.Parameters.AddWithValue("@x", x);
+                        cmd.Parameters.AddWithValue("@y", y);
+                        cmd.Parameters.AddWithValue("@CofI", CofI);
+                        cmd.Parameters.AddWithValue("@CofH", CofH);
+
+                        cmd.ExecuteNonQuery();
+                    }
+
+                    //прошла жара
+
+
+                    cn.Close();
+                }
+                catch (Exception ex)
+                {
+                    System.Console.WriteLine("Cannot connect to db\n\n" + ex.Message);
+                }
+            }
+
+        }
+
+
+
+    }
 }

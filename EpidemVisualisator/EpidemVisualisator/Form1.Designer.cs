9c004c6b4713ce3964c3000c05bb71b0e80212fe
kkrutenyuk <kkrutenyuk@jaggedpeak.com>
2018-12-10 04:13:50 +0200

    Fix loader and add Image

--- EpidemVisualisator/EpidemVisualisator/Form1.Designer.cs
+++ EpidemVisualisator/EpidemVisualisator/Form1.Designer.cs
@@ -28,20 +28,35 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.SuspendLayout();
-            // 
-            // Form1
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(794, 507);
-            this.Name = "Form1";
-            this.Text = "Visualisator";
-            this.ResumeLayout(false);
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.BackgroundImage = global::EpidemVisualisator.Properties.Resources.Novo;
+			this.pictureBox1.Location = new System.Drawing.Point(12, 44);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(946, 403);
+			this.pictureBox1.TabIndex = 0;
+			this.pictureBox1.TabStop = false;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(965, 628);
+			this.Controls.Add(this.pictureBox1);
+			this.Name = "Form1";
+			this.Text = "Visualisator";
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			this.ResumeLayout(false);
 
         }
 
-        #endregion
-    }
+		#endregion
+
+		private System.Windows.Forms.PictureBox pictureBox1;
+	}
 }
 
--- EpidemVisualisator/EpidemVisualisator/Models/Global.cs
+++ EpidemVisualisator/EpidemVisualisator/Models/Global.cs
@@ -70,6 +70,4 @@ namespace EpidemProc.Models
 
 
 	}
-
-
 }
